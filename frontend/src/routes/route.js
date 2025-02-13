d9b14a9357fe7e1331701d3051595c8f60ac803a
Jemimah000 <28dora.nobi@gmail.com>
2025-02-13 15:46:59 +0530

    Initial commit with all files

---
+++ frontend/src/routes/route.js
@@ -0,0 +1,5 @@
+import LoginPage from "../pages/Login";
+import SignupPage from "../pages/SignupPage";
+import Home from "../pages/Home";
+import CreateProduct from "../pages/createProduct";
+export {LoginPage,SignupPage,Home,CreateProduct};
